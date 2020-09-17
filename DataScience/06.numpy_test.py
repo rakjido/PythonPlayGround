fc17d55768b1a34bfc12f5dd1a96bb78ad7c680a
rakjido <ipos222@naver.com>
2020-09-17 15:30:51 +0900

    2020.09.17

--- DataScience/06.numpy_test.py
+++ DataScience/06.numpy_test.py
@@ -1,78 +1,78 @@
 import numpy as np
 
-# print(np.__version__)
+print(np.__version__)
 
-# a = np.array([1,2,3,4])
-# print(a) # 리스트와 구분하기 위해 쉼표가 없이 출력된다 
-# print("a.dtype : {}".format(a.dtype))
-# print("a.ndim : {}".format(a.ndim))
-# print("a.shape : {}".format(a.shape))
-# print("a.itemsize : {}".format(a.itemsize))
-# print("a.strides : {}".format(a.strides))
-# print("a.data : {}".format(a.data))         # 메모리 주소값
-# print("a.data.obj : {}".format(a.data.obj))  # 헤당 매모리 주소값의 참조값
-# print("a.tolist() : {}".format(a.tolist()))  # 리스트로 변환 
-# print("dir(a) : {}".format(dir(a)))
+a = np.array([1,2,3,4])
+print(a) # 리스트와 구분하기 위해 쉼표가 없이 출력된다 
+print("a.dtype : {}".format(a.dtype))
+print("a.ndim : {}".format(a.ndim))
+print("a.shape : {}".format(a.shape))
+print("a.itemsize : {}".format(a.itemsize))
+print("a.strides : {}".format(a.strides))
+print("a.data : {}".format(a.data))         # 메모리 주소값
+print("a.data.obj : {}".format(a.data.obj))  # 헤당 매모리 주소값의 참조값
+print("a.tolist() : {}".format(a.tolist()))  # 리스트로 변환 
+print("dir(a) : {}".format(dir(a)))
 
-# b = np.array([[1,2,3,4],[5,6,7,8]])
-# print(b)
-# print("b.dtype : {}".format(b.dtype))
-# print("b.ndim : {}".format(b.ndim))
-# print("b.shape : {}".format(b.shape))
-# print("b.itemsize : {}".format(b.itemsize))
-# print("b.strides : {}".format(b.strides))
-# print("b.data : {}".format(b.data))         # 메모리 주소값
-# print("b.data.obj : {}".format(b.data.obj))  # 헤당 매모리 주소값의 참조값
-# print("b.tolist() : {}".format(b.tolist()))  # 리스트로 변환 
+b = np.array([[1,2,3,4],[5,6,7,8]])
+print(b)
+print("b.dtype : {}".format(b.dtype))
+print("b.ndim : {}".format(b.ndim))
+print("b.shape : {}".format(b.shape))
+print("b.itemsize : {}".format(b.itemsize))
+print("b.strides : {}".format(b.strides))
+print("b.data : {}".format(b.data))         # 메모리 주소값
+print("b.data.obj : {}".format(b.data.obj))  # 헤당 매모리 주소값의 참조값
+print("b.tolist() : {}".format(b.tolist()))  # 리스트로 변환 
 
-# print("row : {}".format(b.shape[0]))
-# print("coumn : {}".format(b.shape[1]))
+print("row : {}".format(b.shape[0]))
+print("coumn : {}".format(b.shape[1]))
 
-# c = b.copy()
-
-
-# a  = np.array([[1,2,3],[4,5,6],[7,8,9]])
-# print(a)
-
-# # row를 1차원 배열로 반환
-# print("a[0] \n {}".format(a[0]))
-
-# # row를 2차원 배열로 반환
-# print("a[[0]] \n {}".format(a[[0]]))
-
-# # np.array[row, column]
-# print("a[0,2] \n {}".format(a[0,2]))
-
-# # np.array[[row, row]]
-# print("a[[0,2]] \n {}".format(a[[0,2]]) )
-
-# # np.array[:,[0,2]] 이면 0, 2번째 행의 모든 열을 출력
-# print("a[:, [0,2]] \n {}".format(a[:,  [0,2]]))
-
-# # 열과 행에 모두 리스트를 적용하는 것은 제대로 되지 않는다. 이 경우는 열의 조건과 행의 조건을 만족하는 [1 9]가 출력
-# print("a[[0,2], [0,2]] \n {}".format(a[[0,2],  [0,2]]))
-
-# # 열과 행에 모두 리스트를 적용하려면 np.ix_를 사용해야 한다.
-# print("a[np.ix_([0,2],[0,2])] \n {}".format(a[np.ix_([0,2],[0,2])]))
-
-# # 논리 검색시 and, or를 사용할 수 없다. &, |를 사용해야 한다.
-# print("a[a>5] \n {}".format(a[a>5]))
+c = b.copy()
 
 
-# print("a[(a>5) & (a<8)] \n {}".format(a[(a>5) & (a<8) ]))
+a  = np.array([[1,2,3],[4,5,6],[7,8,9]])
+print(a)
 
-# # array간의 결합
-# A = np.arange(1,10).reshape(3,3)
-# print(A)
+# row를 1차원 배열로 반환
+print("a[0] \n {}".format(a[0]))
 
-# B = np.arange(11,14).reshape(1,3)
-# print(B)
+# row를 2차원 배열로 반환
+print("a[[0]] \n {}".format(a[[0]]))
 
-# # vertical 결합 : axis=0
-# print(np.append(A,B, axis=0))
+# np.array[row, column]
+print("a[0,2] \n {}".format(a[0,2]))
 
-# # horizontal 결합 : axis=1
-# print(np.append(A,B.T, axis=1))
+# np.array[[row, row]]
+print("a[[0,2]] \n {}".format(a[[0,2]]) )
+
+# np.array[:,[0,2]] 이면 0, 2번째 행의 모든 열을 출력
+print("a[:, [0,2]] \n {}".format(a[:,  [0,2]]))
+
+# 열과 행에 모두 리스트를 적용하는 것은 제대로 되지 않는다. 이 경우는 열의 조건과 행의 조건을 만족하는 [1 9]가 출력
+print("a[[0,2], [0,2]] \n {}".format(a[[0,2],  [0,2]]))
+
+# 열과 행에 모두 리스트를 적용하려면 np.ix_를 사용해야 한다.
+print("a[np.ix_([0,2],[0,2])] \n {}".format(a[np.ix_([0,2],[0,2])]))
+
+# 논리 검색시 and, or를 사용할 수 없다. &, |를 사용해야 한다.
+print("a[a>5] \n {}".format(a[a>5]))
+
+
+print("a[(a>5) & (a<8)] \n {}".format(a[(a>5) & (a<8) ]))
+
+# array간의 결합
+A = np.arange(1,10).reshape(3,3)
+print(A)
+
+B = np.arange(11,14).reshape(1,3)
+print(B)
+
+# vertical 결합 : axis=0
+print(np.append(A,B, axis=0))
+
+# horizontal 결합 : axis=1
+print(np.append(A,B.T, axis=1))
 
 y = np.linspace(1,10,10)
 print(y)
@@ -119,3 +119,133 @@ print(np.log(y))
 
 # log 10
 print(np.log10(y))
+
+
+a = np.array([[1,2,3],[3,2,1]])
+# unique 값 
+print(np.unique(a))
+# inld(a, b) : a에서 b가 있는지 여부의 1차원 배열
+print(np.in1d(a, 2))
+# isin(a, b) : a에서 b가 있는지 여부의 a의 차원 배열
+print(np.isin(a, 2))
+
+# 차집합 a-b
+b = np.array([[1,2,7],[4,5,6]])
+print(a.shape == b.shape)
+print(np.setdiff1d(a, b))
+
+# 교집합 
+print(np.intersect1d(a, b))
+
+# 합집합 
+print(np.union1d(a, b))
+
+# 대칭차집합 = 합집합 - 교집합
+print(np.setxor1d(a, b))
+
+import matplotlib.pyplot as plt
+
+# random normal distribution
+mu = 0
+sigma =  0.1
+number = 1000000
+rd = np.random.normal(mu, sigma, number)
+print(rd)
+plt.hist(rd)
+plt.xlabel("number")
+plt.ylabel("frequency")
+plt.savefig('normal_dist.png')
+# plt.show()
+plt.close()
+
+# random uniform distribution
+# uniform(low, high, size)
+ud = np.random.uniform(0, 2, 1000000)
+print(ud)
+plt.hist(ud)
+plt.xlabel("number")
+plt.ylabel("frequency")
+plt.savefig('uniform_dist.png')
+plt.close()
+
+a = np.arange(1,10)
+b = np.arange(3,12)
+print(dir(a))
+print(type(a))
+# a의 type이 numpy인지 여부 
+print(type(a).__module__ == np.__name__)
+# a와 b의 size가 같은지 여부
+print(a.shape==b.shape)
+print(a)
+print(b)
+
+# element add
+try :
+    print(a+b)
+except Exception as e:
+    print(e)
+
+# element subtract
+try :
+    print(a-b)
+except Exception as e:
+    print(e)
+
+# element mutiply
+try :
+    print(a*b)
+except Exception as e:
+    print(e)
+
+# inner product
+try :
+    print(np.inner(a, b))
+except Exception as e:
+    print(e)
+
+# dot product
+try :
+    print(np.dot(a, b))
+except Exception as e:
+    print(e)
+
+# dot product
+try :
+    print(a.dot(b))
+except Exception as e:
+    print(e)
+
+A = np.array([[1,2,3],[4,5,6],[7,8,9]])
+B = np.array([[9,8,7],[6,5,4],[3,2,1]])
+try :
+    print(np.dot(A, B))
+except Exception as e:
+    print(e)
+
+# [[ 30  24  18]
+#  [ 84  69  54]
+#  [138 114  90]]
+
+C = np.array([[9,8,7]])
+try :
+    print(np.dot(A, C.T))
+except Exception as e:
+    print(e)
+
+from numpy import linalg as la 
+# determinant
+print(la.det(B))
+
+# ivnerse matrix
+print(la.inv(B))
+
+# identity matrix (단위행렬)
+print(np.eye(3))
+
+# eigen value, eigen vector
+v, w = la.eig(B)
+print(v)
+print(w)
+
+# 0으로 이루어진 행렬
+print(np.zeros((3,7)))
--- DataScience/07.pandas_test.py
+++ DataScience/07.pandas_test.py
@@ -6,134 +6,162 @@ sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
 import pandas as pd 
 import numpy as np
 
-a = pd.Series([1,2,3,4], index=list('abcd'))
-print(a)
-print("a.values : {}".format(a.values))
-print("a.index : {}".format(a.index))
+# a = pd.Series([1,2,3,4], index=list('abcd'))
+# print(a)
+# print("a.values : {}".format(a.values))
+# print("a.index : {}".format(a.index))
 
 
-b = pd.Series([1,2,3,4], index=list(['2020-01-01','2020-01-02', '2020-01-03', '2020-01-04']))
-print(b)
-print("b.values : {}".format(b.values))
-print("b.index : {}".format(b.index))
-print("b.shape : {}".format(b.shape))
-print("b.ndim : {}".format(b.ndim))
-print("b.size : {}".format(b.size))
-print("b[0] : {}".format(b[0]))
-print("b['2020-01-02'] : {}".format(b['2020-01-02']))
-print("b['2020-01-02':'2020-01-04'] : {}".format(b['2020-01-02':'2020-01-04']))
+# b = pd.Series([1,2,3,4], index=list(['2020-01-01','2020-01-02', '2020-01-03', '2020-01-04']))
+# print(b)
+# print("b.values : {}".format(b.values))
+# print("b.index : {}".format(b.index))
+# print("b.shape : {}".format(b.shape))
+# print("b.ndim : {}".format(b.ndim))
+# print("b.size : {}".format(b.size))
+# print("b[0] : {}".format(b[0]))
+# print("b['2020-01-02'] : {}".format(b['2020-01-02']))
+# print("b['2020-01-02':'2020-01-04'] : {}".format(b['2020-01-02':'2020-01-04']))
 
-# 시리즈는 새로운 데이터 추가 가능
-b['2020-01-05'] = 11
-print(b)
+# # 시리즈는 새로운 데이터 추가 가능
+# b['2020-01-05'] = 11
+# print(b)
 
-# dataframe으로 전환
-c = b.to_frame()
-print(type(c))
+# # dataframe으로 전환
+# c = b.to_frame()
+# print(type(c))
 
 
-df = pd.DataFrame([[1,2,3,4],[5,6,7,8],[9,10,11,12]], index=list('abc'), columns=list('ABCD'))
-print(df)
-print("df.values : \n {}".format(df.values))
-print("type(df.values) :  {}".format(type(df.values)))
-print("df.index :  {}".format(df.index))
-print("df.columns :  {}".format(df.columns))
-print("df.shape :  {}".format(df.shape))
-print("rows :  {}".format(df.shape[0]))
-print("columns :  {}".format(df.shape[1]))
-print("df.ndim :  {}".format(df.ndim))
-print("df.size :  {}".format(df.size))
-# column 선택 
-print("df['B'] :  {}".format(df['B']))
-# loc[row index, column index]
-print("df.loc['b'] :  {}".format(df.loc['b']))
-print("df.loc['b','A':'B'] :  {}".format(df.loc['b', 'A':'B']))
+# df = pd.DataFrame([[1,2,3,4],[5,6,7,8],[9,10,11,12]], index=list('abc'), columns=list('ABCD'))
+# print(df)
+# print("df.values : \n {}".format(df.values))
+# print("type(df.values) :  {}".format(type(df.values)))
+# print("df.index :  {}".format(df.index))
+# print("df.columns :  {}".format(df.columns))
+# print("df.shape :  {}".format(df.shape))
+# print("rows :  {}".format(df.shape[0]))
+# print("columns :  {}".format(df.shape[1]))
+# print("df.ndim :  {}".format(df.ndim))
+# print("df.size :  {}".format(df.size))
+# # column 선택 
+# print("df['B'] :  {}".format(df['B']))
+# # loc[row index, column index]
+# print("df.loc['b'] :  {}".format(df.loc['b']))
+# print("df.loc['b','A':'B'] :  {}".format(df.loc['b', 'A':'B']))
 
-df['B'] = 100
+# df['B'] = 100
 
-# iloc[row no, column no]
-print("df.iloc[2] :  {}".format(df.iloc[2]))
-print("df.loc[2,1:2] :  {}".format(df.iloc[2, 1:2]))
+# # iloc[row no, column no]
+# print("df.iloc[2] :  {}".format(df.iloc[2]))
+# print("df.loc[2,1:2] :  {}".format(df.iloc[2, 1:2]))
 
-# 자료형 변환 astype
+# # 자료형 변환 astype
 
-lists = [[1,3],[4,5]]
-columns = ['Math','Eng']
-dt = pd.DataFrame(data=lists, columns=columns) 
-print(dt)
-print("dt.dtypes : \n {}".format(dt.dtypes))
+# lists = [[1,3],[4,5]]
+# columns = ['Math','Eng']
+# dt = pd.DataFrame(data=lists, columns=columns) 
+# print(dt)
+# print("dt.dtypes : \n {}".format(dt.dtypes))
 
-dt2 = dt.astype({'Math': np.int32, 'Eng' : str})
-print("dt2.dtypes \n : {}".format(dt2.dtypes))
+# dt2 = dt.astype({'Math': np.int32, 'Eng' : str})
+# print("dt2.dtypes \n : {}".format(dt2.dtypes))
 
-# Series을 Dataframe으로 변환
+# # Series을 Dataframe으로 변환
 
-dt3 = pd.DataFrame({'A': pd.Series([1,2,3], dtype='int'), 'B': pd.Series([7,8,9], dtype='float')})
-print("dt3.dtypes : \n {}".format(dt3.dtypes))
+# dt3 = pd.DataFrame({'A': pd.Series([1,2,3], dtype='int'), 'B': pd.Series([7,8,9], dtype='float')})
+# print("dt3.dtypes : \n {}".format(dt3.dtypes))
 
-# dictionary형태로 데이터 입력 
-dt4 = pd.DataFrame({'date':['2020-01-01','2020-01-02','2020-01-03','2020-01-04'], 'type':['A','B','C','D']})
-print(dt4)
+# # dictionary형태로 데이터 입력 
+# dt4 = pd.DataFrame({'date':['2020-01-01','2020-01-02','2020-01-03','2020-01-04'], 'type':['A','B','C','D']})
+# print(dt4)
 
-# reshape(row수, column수)
-a = np.arange(10).reshape(2,5)
-print(a)
-df5 = pd.DataFrame(a, columns=list('abcde'))
-print(df5)
+# # reshape(row수, column수)
+# a = np.arange(10).reshape(2,5)
+# print(a)
+# df5 = pd.DataFrame(a, columns=list('abcde'))
+# print(df5)
 
-# df['a'] : array return
-print("df5['a'] \n {}".format(df5['a']))
+# # df['a'] : array return
+# print("df5['a'] \n {}".format(df5['a']))
 
-# df[['a']] : dataframe return
-print("df5[['a']] \n {}".format(df5[['a']]))
-print("df5[['a','c','d']] \n {}".format(df5[['a','c','d']]))
+# # df[['a']] : dataframe return
+# print("df5[['a']] \n {}".format(df5[['a']]))
+# print("df5[['a','c','d']] \n {}".format(df5[['a','c','d']]))
 
-b = a > 5
-# 1차원으로 변환 
-c = b.flatten()
-print(c)
-# array로 return하는 논리연산
-print(df5.values.flatten()[c])
-# dataframe을 return하는 논리연산
-print(df5[df5>5])
+# b = a > 5
+# # 1차원으로 변환 
+# c = b.flatten()
+# print(c)
+# # array로 return하는 논리연산
+# print(df5.values.flatten()[c])
+# # dataframe을 return하는 논리연산
+# print(df5[df5>5])
 
-df1 = pd.DataFrame([[1,2],[3,4]], columns=list('가나'))
-print(df1)
-df2 = pd.DataFrame([[5,6],[7,8]], columns=list('가나'))
-print(df1)
+# df1 = pd.DataFrame([[1,2],[3,4]], columns=list('가나'))
+# print(df1)
+# df2 = pd.DataFrame([[5,6],[7,8]], columns=list('가나'))
+# print(df1)
 
-# append : vertical 결합
-print(df1.append(df2))
-# index를 새로 설정하려면 ignore_index=True
-print(df1.append(df2, ignore_index=True))
+# # append : vertical 결합
+# print(df1.append(df2))
+# # index를 새로 설정하려면 ignore_index=True
+# print(df1.append(df2, ignore_index=True))
 
-# concat : vertical 결합 (axis=0 default)
-print(pd.concat([df1, df2], ignore_index=True))
-# concat : horizonal 결합 (axis=1)
-df3 = pd.DataFrame([[1],[2]], columns=list('다'))
-print(pd.concat([df1,df3], sort=True, axis=1))
+# # concat : vertical 결합 (axis=0 default)
+# print(pd.concat([df1, df2], ignore_index=True))
+# # concat : horizonal 결합 (axis=1)
+# df3 = pd.DataFrame([[1],[2]], columns=list('다'))
+# print(pd.concat([df1,df3], sort=True, axis=1))
 
-# merge : index 또는 공통컬럼을 통한 horizontal 결합 
-df1 = pd.DataFrame({
-    '고객번호': [1001, 1002, 1003, 1004, 1005, 1006, 1007],
-    '이름': ['둘리', '도우너', '또치', '길동', '희동', '마이콜', '영희']
-}, columns=['고객번호', '이름'])
-print(df1)
+# # merge : index 또는 공통컬럼을 통한 horizontal 결합 
+# df1 = pd.DataFrame({
+#     '고객번호': [1001, 1002, 1003, 1004, 1005, 1006, 1007],
+#     '이름': ['둘리', '도우너', '또치', '길동', '희동', '마이콜', '영희']
+# }, columns=['고객번호', '이름'])
+# print(df1)
 
-df2 = pd.DataFrame({
-    '고객번호': [1001, 1001, 1005, 1006, 1008, 1001],
-    '금액': [10000, 20000, 15000, 5000, 100000, 30000]
-}, columns=['고객번호', '금액'])
-print(df2)
-print(pd.merge(df1, df2))
-# 공통컬럼이 여려개일 일 경우 merge할 기준 key 컬럼 지정
-print(pd.merge(df1, df2, on='고객번호'))
+# df2 = pd.DataFrame({
+#     '고객번호': [1001, 1001, 1005, 1006, 1008, 1001],
+#     '금액': [10000, 20000, 15000, 5000, 100000, 30000]
+# }, columns=['고객번호', '금액'])
+# print(df2)
+# print(pd.merge(df1, df2))
+# # 공통컬럼이 여려개일 일 경우 merge할 기준 key 컬럼 지정
+# print(pd.merge(df1, df2, on='고객번호'))
 
-# how='outer' : 공통컬럼이 한쪽에만 있어도 모두 데이터를 보여준다
-print(pd.merge(df1, df2, how='outer'))
+# # how='outer' : 공통컬럼이 한쪽에만 있어도 모두 데이터를 보여준다
+# print(pd.merge(df1, df2, how='outer'))
 
-# how ='letf' : left join
-print(pd.merge(df1, df2, how='left'))
+# # how ='letf' : left join
+# print(pd.merge(df1, df2, how='left'))
 
-# how = 'right' : right join 
-print(pd.merge(df1, df2, how='right'))
+# # how = 'right' : right join 
+# print(pd.merge(df1, df2, how='right'))
+
+url = 'http://www.fdic.gov/bank/individual/failed/banklist.html'
+# read HTML tables into a list of Dataframe objects
+dfs = pd.read_html(url)
+print(type(dfs))
+# list[0]은 dataframe임 
+dfs = dfs[0]
+print(type(dfs))
+print(dfs.head())
+print(dfs.info())
+# covnert to datetime
+dfs['Closing Date'] = pd.to_datetime(dfs['Closing Date'])
+print(dfs.head())
+print(dfs.dtypes)
+dfs['year'] = dfs['Closing Date'].dt.strftime('%Y')
+print(dfs.head())
+# 년도별 합계
+print(dfs['year'].value_counts())
+
+dfs.to_csv("failed_bank_list.csv")
+dfs2 = pd.read_csv("failed_bank_list.csv", parse_dates=['Closing Date'])
+print(dfs2.dtypes)
+
+# url = 'https://dhlottery.co.kr/gameResult.do?method=byWin'
+# dfs = pd.read_html(url)
+# print(dfs)
+# print(dfs[0])
+# print(type(dfs))
